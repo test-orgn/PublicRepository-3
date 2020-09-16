cec91e7ba6030b5bf23f1189d87f8f41d2b8c093
ConsolePeasant92 <hugocornel@yahoo.com>
2020-09-16 16:17:16 +0200

    small S3 correction

--- DP/SQL/Opdracht 3/1.sql
+++ DP/SQL/Opdracht 3/1.sql
@@ -3,6 +3,6 @@
 -- cursuscode, de begindatum, de cursuslengte en de naam van de docent.
 -- DROP VIEW IF EXISTS s3_1; CREATE OR REPLACE VIEW s3_1 AS                                                     -- [TEST]
 
-SELECT uitvoeringen.cursus, uitvoeringen.begindatum, cursussen.lengte, medewerkers.naam FROM uitvoeringen
-INNER JOIN cursussen ON uitvoeringen.cursus=cursussen.code
-INNER JOIN medewerkers ON uitvoeringen.docent=medewerkers.mnr;
+SELECT u.cursus, u.begindatum, c.lengte, m.naam FROM uitvoeringen AS u
+INNER JOIN cursussen AS c ON u.cursus=c.code
+INNER JOIN medewerkers AS m ON u.docent=m.mnr;
