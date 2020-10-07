ee4775ed86ff3bd0b59d1fac82a9eb683eac6afd
ConsolePeasant92 <hugocornel@yahoo.com>
2020-10-07 14:39:04 +0200

    small correction

--- DP/SQL/Opdracht 7/3c.sql
+++ DP/SQL/Opdracht 7/3c.sql
@@ -2,13 +2,9 @@
 --
 -- Zou je de query ook heel anders kunnen schrijven om hem te versnellen?
 
--- Je zou een JOIN kunnen gebruiken in plaats van een subquery voor het berekenen van de gemiddelde levertijd. Dit was maar een paar honderd milliseconden 
--- sneller. Toen heb ik het geprobeerd met GROUP BY en HAVING, alleen daar kreeg ik maar 275 rijen terug (ipv 377).
-
 SELECT o.order_id, o.order_date, o.salesperson_person_id AS verkoper, 
 DATE_PART('day', AGE(o.expected_delivery_date, o.order_date)) AS levertijd, ol.quantity
-FROM orders AS o
-JOIN order_lines AS ol ON o.order_id = ol.order_id
-WHERE ol.quantity > 250
-GROUP BY o.order_id, o.order_date, verkoper, levertijd, ol.quantity
-HAVING AVG(DATE_PART('day', AGE(expected_delivery_date, order_date))) > 1.45;
+FROM orders AS o, order_lines AS ol
+WHERE o.order_id = ol.order_id AND ol.quantity > 250 AND o.salesperson_person_id IN
+(SELECT sq.salesperson_person_id FROM orders as sq GROUP BY sq.salesperson_person_id 
+HAVING AVG(DATE_PART('day', AGE(sq.expected_delivery_date, sq.order_date))) > 1.45);
